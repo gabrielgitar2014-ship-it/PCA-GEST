3aaddb328c0fb3872f28709b4f870dc65092f96b
gabrielgitar2014-ship-it <gabriel.gitar2014@gmail.com>
2025-10-14 18:18:24 -0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-// DENTRO DE: src/pages/Dashboard.jsx
+5// DENTRO DE: src/pages/Dashboard.jsx
 
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
@@ -52,7 +52,7 @@ export default function Dashboard() {
         {/* Coluna Principal (à esquerda) */}
         <div className="lg:col-span-2 space-y-6">
           
-          <div className="bg-white rounded-lg shadow-sm p-6 border-l-4 border-red-500">
+          <div className="bg-white rounded-lg shadow-sm p-6 border-l-4 border-yellow-500">
             <div className="flex items-center space-x-3 mb-4">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
               <h2 className="text-xl font-bold text-slate-800">Casos para Análise (Desencadeamento)</h2>
@@ -104,3 +104,4 @@ export default function Dashboard() {
   );
 
 }
+
